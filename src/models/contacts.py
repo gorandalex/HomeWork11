a8ad60bfbff94f69754f563afd369bb1ba3c39ad
gorandalex <andrii.gorobets@gmail.com>
2023-03-10 12:30:03 +0200

    add authorization

--- src/models/contacts.py
+++ src/models/contacts.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Date, Integer, String
+import enum
+from sqlalchemy import Column, Integer, String, DateTime, func, Enum, Date
 from sqlalchemy.orm import declarative_base
 
 
@@ -7,7 +8,7 @@ Base = declarative_base()
 
 class Contact(Base):
     __tablename__ = "contacts"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     first_name = Column(String, index=True, nullable=False)
     last_name = Column(String, index=True, nullable=False)
     email = Column(String, index=True, nullable=False)
@@ -15,3 +16,18 @@ class Contact(Base):
     birthday = Column(Date, index=True, nullable=False)
     description = Column(String)
 
+class Roles(enum.Enum):
+    admin: str = "admin"
+    moderator: str = "moderator"
+    user: str = "user"
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('created_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
+    roles = Column('role', Enum(Roles), default=Roles.user)
